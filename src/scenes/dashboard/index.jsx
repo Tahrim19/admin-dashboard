11810b803043852b8a1c6f4dc17ea618181a1840
Tahrim19 <tarimbilal4@gmail.com>
2024-12-09 22:36:22 +0500

    Header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
 export default function Dashboard() {
   return (
-    <div>
-      dashboard
-    </div>
-  )
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard"></Header>
+      </Box>
+    </Box>
+  );
 }
